38f388d4f8b7d724cbeeb7aa132ebc3116a74f80
boohofwooh <104630049+boohofwooh@users.noreply.github.com>
2023-05-30 00:26:20 +0700

    Update test.php
    29 Mei 2023

--- test.php
+++ test.php
@@ -9,3 +9,35 @@ if($panjangNama == (date('d')-2)){
 } else {
     echo "Coba Lagi";
 }
+
+//Latihan 2
+$member = array("Salma" => "494348", "Jamal" => "899320", "Milah" => "712189");
+foreach($member as $name => $nim) {
+    if($nim % 2 == 0){
+        echo "Nama : " . $name . "<br>";
+        echo "NIM : " . $nim . "<br>";
+        echo "Bagian : Back-End Developer" . "<br><br>";
+    } else {
+        echo "Nama : " . $name . "<br>";
+        echo "NIM : " . $nim . "<br>";
+        echo "Bagian : Front-End Developer" . "<br><br>";}
+}
+
+//Latihan 3
+$name = "Zalfa";
+$city = array("Lampung", "Riau", "Jambi", "Bengkulu", "Makassar", "Kendari", "Gorontalo", "Samarinda", "Papua", "Nusa Tenggara Barat");
+$array = array();
+for ($i = 0; $i < strlen($name); $i++){
+    $searchcity = strtolower($name[$i]);
+    for($j = 0; $j < count($kota); $j++){
+        $addcity = strtolower($city[$j]);
+        if ($searchcity == $addcity[0]){
+            array_push($array, $city[$y]);
+        }
+    }
+}
+if(count($array)==0){
+    print_r("Anda akan ditempatkan di Jawa Timur");
+} else {
+    print_r("Anda akan ditempatkan di ".$array[0]);
+}
